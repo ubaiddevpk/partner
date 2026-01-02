03f44ae1959cad7d70d464506feb64b3ba5fe678
Obaid ullah <obaidullah.developer@gmail.com>
2026-01-02 16:47:39 +0500

    add secret key to env file

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://tpwonzripeslsvzdchti.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwd29uenJpcGVzbHN2emRjaHRpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjY3MjYzMjIsImV4cCI6MjA4MjMwMjMyMn0.hqSzVW3GQTEjxbBzrtriEcc_AmIWMBY9tIQDVGOzF64'
+// Use environment variables
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
